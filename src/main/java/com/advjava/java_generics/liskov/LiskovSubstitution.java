bbf87cca236d1f01885a05629a015f60732b7afb
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2024-01-03 14:11:30 -0300

    Updates ClothingSite

--- src/main/java/com/advjava/java_generics/liskov/LiskovSubstitution.java
+++ src/main/java/com/advjava/java_generics/liskov/LiskovSubstitution.java
@@ -1,24 +1,15 @@
 package com.advjava.java_generics.liskov;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
 import com.advjava.java_generics.liskov.clothing_store.ClothingSite;
-import com.advjava.java_generics.liskov.clothing_store.clothes.ClothingItem;
 import com.advjava.java_generics.liskov.clothing_store.clothes.JacketItem;
 import com.advjava.java_generics.liskov.clothing_store.clothes.ShirtItem;
 
 public class LiskovSubstitution {
 
-    private static List<ClothingItem> clothes = Arrays.asList(
-            new JacketItem(),
-            new ShirtItem());
-
     public static void main(String[] args) {
-
-        for (ClothingItem item : clothes) {
-            ClothingSite.checkoutItem(item);
-        }
+        ClothingSite.checkoutItems(Arrays.asList(
+                new JacketItem(),
+                new ShirtItem()));
     }
 }
--- src/main/java/com/advjava/java_generics/liskov/clothing_store/ClothingSite.java
+++ src/main/java/com/advjava/java_generics/liskov/clothing_store/ClothingSite.java
@@ -1,9 +1,18 @@
 package com.advjava.java_generics.liskov.clothing_store;
 
+import java.util.List;
+
 import com.advjava.java_generics.liskov.clothing_store.clothes.ClothingItem;
 
 public class ClothingSite {
-    public static void checkoutItem(ClothingItem item) {
+
+    public static void checkoutItems(List<ClothingItem> clothes) {
+        for (ClothingItem item : clothes) {
+            checkoutItem(item);
+        }
+    }
+
+    private static void checkoutItem(ClothingItem item) {
         System.out.println("Item purchased: " + item.getName() + ", price: " + item.getPrice());
     }
 }
